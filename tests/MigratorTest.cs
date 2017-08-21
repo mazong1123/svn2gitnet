e60b09f56d05922ea367ff91dfea4868c9c50b3f
mazong1123 <mazong1123@gmail.com>
2017-08-21 22:19:36 +0800

    Added more unit tests.

--- tests/MigratorTest.cs
+++ tests/MigratorTest.cs
@@ -12,12 +12,72 @@ namespace Svn2GitNet.Tests
     public class MigratorTest
     {
         [Fact]
-        public void MissingSvnUrlParameterWhenInitializeTest()
+        public void EmptyParameterInitializeTest()
         {
             // Prepare
             string[] args = new string[] { };
             Migrator migrator = new Migrator(new Options(), args);
             MigrateResult expected = MigrateResult.MissingSvnUrlParameter;
+
+            // Act
+            MigrateResult actual = migrator.Initialize();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void RebaseWithExtraArgumentsInitializeTest()
+        {
+            // Prepare
+            string[] args = new string[] { "--rebase", "extraarg" };
+            Migrator migrator = new Migrator(new Options(), args);
+            MigrateResult expected = MigrateResult.TooManyArguments;
+
+            // Act
+            MigrateResult actual = migrator.Initialize();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void RebaseBranchWithExtraArgumentsInitializeTest()
+        {
+            // Prepare
+            string[] args = new string[] { "--rebasebranch", "extraarg" };
+            Migrator migrator = new Migrator(new Options(), args);
+            MigrateResult expected = MigrateResult.TooManyArguments;
+
+            // Act
+            MigrateResult actual = migrator.Initialize();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SvnUrlWithExtraArgumentsInitializeTest()
+        {
+            // Prepare
+            string[] args = new string[] { "http://testsvnurl", "extraarg" };
+            Migrator migrator = new Migrator(new Options(), args);
+            MigrateResult expected = MigrateResult.TooManyArguments;
+
+            // Act
+            MigrateResult actual = migrator.Initialize();
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SvnUrlInitializeSuccessTest()
+        {
+            // Prepare
+            string[] args = new string[] { "http://testsvnurl" };
+            Migrator migrator = new Migrator(new Options(), args);
+            MigrateResult expected = MigrateResult.OK;
 
             // Act
             MigrateResult actual = migrator.Initialize();
