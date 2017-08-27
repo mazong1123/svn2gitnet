eacb5ba00ed0e98aee4d712228ff4ca0cff60028
mazong1123 <mazong1123@gmail.com>
2017-08-27 14:22:53 +0800

    Updated publish scripts.

--- publish.sh
+++ publish.sh
@@ -2,5 +2,4 @@ cd src
 dotnet restore
 dotnet publish --self-contained -c Release -r ubuntu.16.04-x64
 dotnet publish --self-contained -c Release -r ubuntu.15.10-x64
-dotnet publish --self-contained -c Release -r win7-x64
 cd ..
